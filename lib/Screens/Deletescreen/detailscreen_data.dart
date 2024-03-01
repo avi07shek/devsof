ff2998ca6ae2cc1129f067b4a5b387188f739854
avi07shek <shahavishek0003@gmail.com>
2024-03-01 09:07:49 +0545

    new file has been added

---
+++ lib/Screens/Deletescreen/detailscreen_data.dart
@@ -0,0 +1,113 @@
+import '../../Model/SingleProductModel.dart';
+
+List<SingleProductModel> detailScreenData = [
+  SingleProductModel(
+    productImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671615614/He93aaf5e6d23419793d6834c969b36c75.jpg_300x300_drlhwo.jpg',
+    productModel: 'Casual',
+    productName:
+        '2020 New Men Casual Shoes Lace up Men Shoes Lightweight Comfortable Breathable Walking Sneakers',
+    productOldPrice: 200,
+    productPrice: 150,
+    productFourImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671615774/H29ef8be4159147299152de836d31e5c7h.jpg_300x300_a3fulk.jpg',
+    productSecondImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671615915/H7e62d2b6f2a1457b88339f3ececbfe37O.jpg_300x300_pmt0aw.jpg',
+    productThirdImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671616060/H5974382385a149768efdf5e773e451f0n.jpg_300x300_fapm6a.jpg',
+  ),
+  SingleProductModel(
+    productImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/c_scale,w_720/v1671268800/Ha5f14e3df9374678933ee53446b84c00D.jpg_720x720q50_s26em3.png',
+    productModel: 'Custom Fashion',
+    productName:
+        '2022 Custom Fashion 3D Embossed T-shirt Tee Premium Men Organic Pima Cotton White Oversize Plain Graphic Custom Embossed Tshirts',
+    productOldPrice: 40,
+    productPrice: 20,
+    productFourImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671618987/H0c417231587644a8ab7c6a11d65ab667L.jpg_300x300_u1znhb.jpg',
+    productSecondImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671619008/Hdfed1b34c338431cab95f15c27be33b0x.jpg_300x300_pqplti.jpg',
+    productThirdImage:
+        'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671619032/Hd52706a95fe84c4a90ce8fe0b2dc08bay.jpg_300x300_dj7stz.jpg',
+  ),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627770/H51dc1a16a9f24606a1f7b08f913633aej.jpg_300x300_pzsx49.png',
+      productModel: 'Tank-tops',
+      productName: 'Workout_Ready_Tech_Tee_Orange',
+      productOldPrice: 30,
+      productPrice: 20,
+      productFourImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627785/H0f9cb76fe8424f55b92a8bd5fd1ec491R.jpg_300x300_maovf2.jpg',
+      productSecondImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627800/Hd04131b33b904dd2a777a0edae6e71160.png_300x300_zl1tql.png',
+      productThirdImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627826/He35434417ee54b41a2696290d44ac29aN.jpg_300x300_asu8sd.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/eb31ff6778fa42e89e57ac480039b8fd_9366/Classics_Archive_Backpack_Small_Black_GN7640.jpg',
+      productModel: 'Classic',
+      productName: 'Classics archive backpack',
+      productOldPrice: 70,
+      productPrice: 54,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/4c3801f601b647279640aaca0105c7f1_9366/Classics_Archive_Backpack_Black_FL7045_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/f509c6c223e947798d50ab3601497153_9366/Classics_Archive_Backpack_Black_GD1040_01_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/53b4eac625434cffbfc1ab1400e1f2bc_9366/Classics_Archive_Backpack_White_FL7044_04_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671270673/Hecd43f51a4d049969f09b62ff4dc2a8b0.jpg_300x300_hm0uwc.jpg',
+      productModel: 'sweatpants',
+      productName:
+          'Custom logo sweatpants embroidered plain walk men jogging pants blank women track pants white fleece mens joggers pants',
+      productOldPrice: 85,
+      productPrice: 70,
+      productFourImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671620837/H6a5f250ed86343698c966f2287c54168L.jpg_300x300_shjm72.jpg',
+      productSecondImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671620863/H706708aa1d8445eda9f35060ca7e94bbf.jpg_300x300_zr8r8m.jpg',
+      productThirdImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671620736/Hcf56474c023f4771841c4e78e468b69ag.jpg_300x300_ioddbh.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/5adc6dabf52a4b23a97baa060176638a_9366/Rivalry_RM_Low_Shoes_Beige_EE4989_01_standard.jpg',
+      productModel: 'Sneakers',
+      productName: 'Rivalry RM Low',
+      productOldPrice: 158,
+      productPrice: 130,
+      productFourImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/590d3dfc6f7c43f2a492aa070012601b_9366/Rivalry_RM_Low_Shoes_Beige_EE4989.jpg',
+      productSecondImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/64515af34c5844289b5daa060178bd6f_9366/Rivalry_RM_Low_Shoes_Beige_EE4989_05_standard.jpg',
+      productThirdImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/991b7aee890647449e80aa06017ee41c_9366/Rivalry_RM_Low_Shoes_Beige_EE4989_03_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/ec37efac818e47f2b522ab61014bccb2_9366/Active_Enhanced_Convertible_Grip_Bag_Grey_GN4963_02_standard_hover.jpg',
+      productModel: 'training',
+      productName: 'Active Enhanced Convertible Grip Bag',
+      productOldPrice: 85,
+      productPrice: 70,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/9d2ce122e3bd4b838e9dabc100ea51b9_9366/Active_Enhanced_Convertible_Grip_Bag_Orange_GH0112_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/dd709bb49870430bb2dfab0500bf9e76_9366/Active_Enhanced_Grip_Bag_Black_FQ5368_04_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/d280d49fa0e8400da15ca9880020b2e0_9366/Active_Enhanced_Grip_Bag_Large_Black_DU2968_04_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627329/Hcfa1a025348c4d2cb34d5a4f0a0ed4b2S.jpg_300x300_krkphr.jpg',
+      productModel: 'jacket',
+      productName: 'Classics vector track jacket',
+      productOldPrice: 100,
+      productPrice: 90,
+      productFourImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627348/H8bfb4c35d3f341968e14ac5af972c262H.jpg_300x300_c9wcft.jpg',
+      productSecondImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627365/H852755451db14e34a1bf769ebdbbe38dT.jpg_300x300_wezohz.jpg',
+      productThirdImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671627378/He7d92a1ab551471e9edd2e01a422dc04s.jpg_300x300_b4f073.jpg'),
+];
